1df11b9262802a6e2de4df49143e9fe8a5e29361
hugosenari <hugosenari@gmail.com>
2022-03-12 02:10:57 -0300

    chore: re index wstp

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Nim bindings for WSTP'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."wstp-master".url = "path:./master";
+  inputs."wstp-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."wstp-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = ''Nim bindings for WSTP'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-wstp-master.flake = false;
+  inputs.src-wstp-master.owner = "oskca";
+  inputs.src-wstp-master.ref   = "refs/heads/master";
+  inputs.src-wstp-master.repo  = "nim-wstp";
+  inputs.src-wstp-master.type  = "github";
+  
+  inputs."c2nim".dir   = "nimpkgs/c/c2nim";
+  inputs."c2nim".owner = "riinr";
+  inputs."c2nim".ref   = "flake-pinning";
+  inputs."c2nim".repo  = "flake-nimble";
+  inputs."c2nim".type  = "github";
+  inputs."c2nim".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."c2nim".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-wstp-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-wstp-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
